43516a9537b145987ed131116f58eb3c15fbc37e
Hal Williams <90720910+hfw8271@users.noreply.github.com>
2024-04-14 23:28:53 -0400

    Create matrixConvert.py

---
+++ code/matrixConvert.py
@@ -0,0 +1,36 @@
+#!/bin/usr/python3
+import os
+from PIL import Image
+import numpy as np
+
+#used to convert the given image to a matrix
+def convert_image_to_matrix(image_path):
+    #opens the image
+    img = Image.open(image_path)
+    #converts to grayscale  (ensures the image is grayscale)
+    img_gray = img.convert('L')
+    #Converts the grayscale to a numpy array
+    img_matrix = np.array(img_gray)
+    return img_matrix
+
+#saves the matrix to a file
+def save_matrix_to_file(matrix, file_path):
+    #saves the matrix file
+    np.save(file_path, matrix)
+
+def process_directory(input_directory, output_directory):
+    #gets all the files in the directory
+    files = os.listdir(input_directory)
+    for file in files:
+        #ensures the file is an image
+        if file.lower().endswith(('.png')):
+            image_matrix = convert_image_to_matrix(os.path.join(input_directory, file))
+            save_matrix_to_file(image_matrix, os.path.join(output_directory, file + '.npy'))
+
+
+#should have all the images you want processed
+input_directory="/home/student/lab4/NISTGrayScaleImages/sd04/png_txt/figs_7"
+#where all the matrix files will be stored for later use
+output_directory="/home/student/lab4/NISTGrayScaleImages/sd04/matrix/figs_7"
+
+process_directory(input_directory, output_directory)
