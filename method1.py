06d679371c84af19a4121dc69dd0e588827eb56a
Alyssa Rios <alyssarios@alyssas-mbp-3.wireless.rit.edu>
2024-04-14 21:22:36 -0400

    update...

---
+++ method1.py
@@ -0,0 +1,20 @@
+#### This is for method 1 ####
+
+####
+import numpy as np
+####
+
+
+
+
+def main():
+    """
+    Fill in with code
+    """
+    for x in range (1500):
+        return
+
+    return
+
+if __name__ == "__main__":
+    main()
--- minutiae-extraction.py
+++ minutiae-extraction.py
@@ -1,1 +1,4 @@
 #### This file will house the code to exrtact minutiae from our fingerprints for our methods to use ####
+
+def extract_minutiae():
+    return
