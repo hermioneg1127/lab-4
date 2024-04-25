39fcdab5a01c119ceaa28d7608c6117013960a16
Alyssa Rios <alyssarios@alyssas-mbp-3.wireless.rit.edu>
2024-04-25 16:34:59 -0400

    Updates to method 1

--- code/method1.py
+++ code/method1.py
@@ -1,20 +1,39 @@
 #### This is for method 1 ####
 
-####
-import numpy as np
-####
+from PIL import Image, ImageChops, ImageStat
+import glob
 
-
+def compare_images(imagef, images):
+    return
 
 
 def main():
-    """
-    Fill in with code
-    """
-    for x in range (1500):
-        return
+    test = [] #initialize test list
+    train = [] #initialize train list
+    for x in range (6): 
+        """
+        populate the train list with all training documents
+        """
+        filepath = "../lab-4/data/NISTSpecialDatabase4GrayScaleImagesofFIGS/sd04/png_txt/figs_" + str(x) + "/*.png"
+        images = glob.glob(filepath)
+        for file in images:
+            train.append(file)
+    for x in range (6,8):
+        """
+        populate the test list with all training documents
+        """
+        filepath = "../lab-4/data/NISTSpecialDatabase4GrayScaleImagesofFIGS/sd04/png_txt/figs_" + str(x) + "/*.png"
+        images = glob.glob(filepath)
+        for file in images:
+            test.append(file)
+            
+        
 
-    return
+
+    # for x in range (1500):
+    #     compare_images(imagef, images)
+
+    # return
 
 if __name__ == "__main__":
     main()
--- code/minutiae-extraction.py
+++
@@ -1,4 +0,0 @@
-#### This file will house the code to exrtact minutiae from our fingerprints for our methods to use ####
-
-def extract_minutiae():
-    return
